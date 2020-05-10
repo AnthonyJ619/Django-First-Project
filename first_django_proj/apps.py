ec1f1ce2cdbe5719509473b740224a8599e54892
Tony Jacobs <usmc6@MacBook-Air.local>
2020-05-09 23:04:15 -0400

    new repo

---
+++ first_django_proj/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class FirstDjangoProjConfig(AppConfig):
+    name = 'first_django_proj'
---
+++ first_django_proj/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.index), 
+    path('new', views.new),
+    path('create', views.create),
+    path('<number>', views.show),
+    path('<number>/edit', views.edit),
+    path('<number>/delete',views.destroy),
+]
